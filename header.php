e15c9a69670955f64bf537d1ee9bd71ee796c060
kervisvasquez1993 <kervisvasquez24@gmail.com>
2020-07-28 16:59:20 -0400

    comenzamos de crear el header

--- header.php
+++ header.php
@@ -23,37 +23,57 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'dynalab' ); ?></a>
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'test' ); ?></a>
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$dynalab_description = get_bloginfo( 'description', 'display' );
-			if ( $dynalab_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $dynalab_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+            <div class="site-title flex-column alert-primary">
+				<div class="header1 header-iconos ">
+            	      <span>
+				         <img  class="logo-header" src="<?php echo get_stylesheet_directory_uri().'/assets/buscar-header.png';?>" alt="">   
+				      </span>
+      
+				      <div class="children-iconos-header">
+      
+      
+				           <span class="wrapp-icon">
+				              <img  class="logo-header" src="<?php echo get_stylesheet_directory_uri().'/assets/telefono-header.png';?>" alt="">
+				           	<p class="font-header"> (+58) 424 4444161 </p>
+				           </span>
+				           
+				           
+				           <span class="wrapp-icon">
+				              <img  class="logo-header" src="<?php echo get_stylesheet_directory_uri().'/assets/correo-header.png';?>" alt="">
+				              <p class="font-header"> atencion@dynamics.com </p>
+				           </span>
+				      </div>
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'dynalab' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
+                </div>
+				<div class="header2">
+				    <?php the_custom_logo();  ?><!-- invocar el logo .site-branding -->
+                       <nav id="site-navigation" class="main-navigation">
+       
+			            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu', 'dynalab' ); ?></button>
+			       
+			       
+			                   <?php
+			                wp_nav_menu(
+			                	array(
+			                		'theme_location' => 'menu-1',
+			                		'menu_id'        => 'primary-menu',
+			                	)
+			                );
+			            
+			                   ?>
+				       </nav><!-- #site-navigation -->
+				</div>
+
+
+            	
+			</div>
+			
+		</div> <!--fin de site-branding-->
+		
+		
+		
 	</header><!-- #masthead -->
